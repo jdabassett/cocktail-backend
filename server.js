f119d5f46c376fb7e7a10ef4eb081a5164d37bbc
jdabassett <jacobbassett@gmail.com>
2023-08-24 23:50:04 -0600

    error handlers added

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ const verifyUser = require("./authorize.js");
 const PORT = process.env.PORT;
 const MONGODB_KEY = process.env.MONGODB_KEY;
 
-var whitelist = ['http://localhost:3000/','http://localhost:3001/', 'https://thriving-crisp-134854.netlify.app/'];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-};
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -32,30 +21,6 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("Mongoose is connected!"));
 
 
-app.get("/", (req, res) => {
-  res.status(200).send({
-    drinks: [
-      {
-        strDrink: "Vodka Slime",
-        strDrinkThumb:
-          "https://www.thecocktaildb.com/images/media/drink/apex461643588115.jpg",
-        idDrink: "178362",
-      },
-      {
-        strDrink: "Caribbean Orange Liqueur",
-        strDrinkThumb:
-          "https://www.thecocktaildb.com/images/media/drink/qwxuwy1472667570.jpg",
-        idDrink: "12796",
-      },
-      {
-        strDrink: "57 Chevy with a White License Plate",
-        strDrinkThumb:
-          "https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg",
-        idDrink: "14029",
-      },
-    ],
-  });
-});
 
 app.use(verifyUser);
 
