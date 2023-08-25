1d472155cb5a3babddf5298c53987c76cd066a23
jdabassett <jacobbassett@gmail.com>
2023-08-24 18:38:07 -0600

    cached database get all user cocktails

--- modules/databaseHandler.js
+++ modules/databaseHandler.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const cocktailModel = require('./cocktailModel.js');
+const cache = require('./cache.js');
 
 
 const databaseHandler = {};
@@ -9,22 +10,46 @@ const databaseHandler = {};
 databaseHandler.userCocktails = (req,res) => {
   //TODO: must change from query to user once verification is working
   let queryObject = {strUserEmail:req.headers.email};
+  let queryKey = `userCocktails:${req.headers.email}`;
 
   let now=new Date; 
   console.log('database-getAll',now.toString());
 
-  cocktailModel
+  if (cache[queryKey] && Date.now() - cache[queryKey].timestamp < 1000000){
+    console.log('database-userCocktails:past');
+    res.status(200).send({ drinks: cache[queryKey].data });
+  } else {
+    console.log('database-userCocktail:now');
+    cocktailModel
     .find(queryObject)
     .then(response =>{
       // console.log(response);
+      cache[queryKey] = {};
+        cache[queryKey].timestamp = Date.now();
+        cache[queryKey].data = response;
+        // console.log(response);
       res.status(200).send({drinks:response});})
     .catch(error=> res.status(500).send({error:error.message}))
+  };
+
+      // cocktailModel
+      //   .find(queryObject)
+      //   .then(response =>{
+      //     res.status(200).send({drinks:response});})
+      //   .catch(error=> res.status(500).send({error:error.message}))
 };
 
 databaseHandler.createCocktail = (req, res) => {
   //TODO: must change from query to user once verification is working
   const createObject = { ...req.body, strUserEmail: req.headers.email };
-  console.log("database-post")
+  let queryKey = `userCocktails:${req.headers.email}`;
+
+  if(cache[queryKey]){
+    delete cache[queryKey];
+  };
+
+  console.log("database-post");
+
   cocktailModel
     .create(createObject)
     .then((response) => res.status(201).send(response))
@@ -35,7 +60,14 @@ databaseHandler.updateCocktail=(req,res)=>{
   //TODO: must change from query to user once verification is working
   let id = req.params.id;
   let updateObject = {...req.body,strUserEmail: req.headers.email};
-  console.log("database-put")
+  let queryKey = `userCocktails:${req.headers.email}`;
+
+  if(cache[queryKey]){
+    delete cache[queryKey];
+  };
+
+  console.log("database-put");
+
   cocktailModel
     .findByIdAndUpdate(id,updateObject,{new:true,overwrite:true})
     .then((response)=> res.status(201).send(response))
@@ -44,8 +76,14 @@ databaseHandler.updateCocktail=(req,res)=>{
 
 databaseHandler.deleteCocktail=(req,res)=>{
   let id = req.params.id;
+  let queryKey = `userCocktails:${req.headers.email}`;
+
+  if(cache[queryKey]){
+    delete cache[queryKey];
+  };
 
   console.log('database-delete',id);
+
   cocktailModel
     .findByIdAndDelete(id)
     .then(response=>res.status(204).send(response))
